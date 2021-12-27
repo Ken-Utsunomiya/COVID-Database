2c830050d190c83a94c2c8fbbcb4dc76a7e3d96a
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-12-27 13:56:51 -0800

    feat: add database

---
+++ src/database/database.go
@@ -0,0 +1,53 @@
+package database
+
+import (
+	"COVID-Database/src/utils"
+	"database/sql"
+	"github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+type Database struct {
+	*gorm.DB
+}
+
+var DB *gorm.DB
+
+func Init() *gorm.DB {
+	dbUri := utils.GetEnvVariable("DB_URI")
+	connection := getConn(dbUri)
+
+	sqlDB, sqlErr := sql.Open("postgres", connection)
+	if sqlErr != nil {
+		log.Fatalf("Error opening sql database")
+	}
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+
+	gormDB, gormErr := gorm.Open(postgres.New(
+		postgres.Config{ Conn: sqlDB }),
+		&gorm.Config{ SkipDefaultTransaction: true},
+	)
+	if gormErr != nil {
+		log.Fatalf("Error opening gorm database")
+	}
+
+	DB = gormDB
+	return DB
+}
+
+func GetDB() *gorm.DB {
+	return DB
+}
+
+func getConn(dbUri string) string {
+	connection, err := pq.ParseURL(dbUri)
+	if err != nil {
+		log.Fatalf("Connection error")
+	}
+	connection += " sslmode=require"
+	return connection
+}
