3b97dfdcc08268d53c000d3dcb49b8529023bc7b
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-12-27 19:31:05 -0800

    feat: add hospital service

--- src/services/hospital.go
+++ src/services/hospital.go
@@ -1,7 +1,18 @@
 package services
 
+import (
+	"github.com/projects/COVID_Database/src/database"
+	"github.com/projects/COVID_Database/src/models"
+)
+
 type HospitalService struct{}
 
-func (HospitalService) GetHospitals() () {
+func (HospitalService) GetHospitals() ([]models.Hospital, error) {
 
+	db := database.GetDB()
+
+	hospitals := make([]models.Hospital, 0)
+	err := db.Find(&hospitals).Error
+
+	return hospitals, err
 }
