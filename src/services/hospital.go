170800c9ca47a03aa69e18b04750530d91e451c8
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-12-28 11:28:45 -0800

    feat: add GetHospital

--- src/services/hospital.go
+++ src/services/hospital.go
@@ -16,3 +16,13 @@ func (HospitalService) GetHospitals() ([]models.Hospital, error) {
 
 	return hospitals, err
 }
+
+func (HospitalService) GetHospital(id int) (models.Hospital, error) {
+
+	db := database.GetDB()
+
+	hospital := models.Hospital{}
+	err := db.First(&hospital, id).Error
+
+	return hospital, err
+}
