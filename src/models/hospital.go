865d7eafba251d8dce16b1e28366acf0644da8b2
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-12-27 19:28:26 -0800

    feat: add hospital model

---
+++ src/models/hospital.go
@@ -0,0 +1,13 @@
+package models
+
+type Hospital struct {
+	ID       int    `json:"hospital_id"`
+	Address  string `json:"address"`
+	Name     string `json:"name"`
+	Tel      string `json:"tel"`
+	Capacity int    `json:"capacity"`
+}
+
+func (Hospital) TableName() string {
+	return "hospitals"
+}
---
+++ src/services/hospital.go
@@ -0,0 +1,7 @@
+package services
+
+type HospitalService struct{}
+
+func (HospitalService) GetHospitals() () {
+
+}
