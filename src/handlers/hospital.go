b6b1d967287a627829327eb2e6aa188009b060eb
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-12-28 11:25:01 -0800

    feat: add HospitalRetrieve

--- src/handlers/hospital.go
+++ src/handlers/hospital.go
@@ -4,11 +4,13 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/projects/COVID_Database/src/services"
 	"net/http"
+	"strconv"
 )
 
 func HospitalList(c *gin.Context) {
 
 	hospitalService := services.HospitalService{}
+
 	hospitalList, err := hospitalService.GetHospitals()
 	if err != nil {
 		_ = c.Error(err).SetType(gin.ErrorTypePublic)
@@ -19,4 +21,17 @@ func HospitalList(c *gin.Context) {
 		"hospitals": hospitalList,
 	})
 
+}
+
+func HospitalRetrieve(c *gin.Context) {
+
+	hospitalService := services.HospitalService{}
+
+	hospitalId, _ := strconv.Atoi(c.Param("hospital_id"))
+	hospital, err := hospitalService.GetHospital(hospitalId)
+	if err != nil {
+		_ = c.Error(err).SetType(gin.ErrorTypePublic)
+	}
+
+	c.JSON(http.StatusOK, hospital)
 }
