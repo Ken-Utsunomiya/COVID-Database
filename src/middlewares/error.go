14ecb6c1ea163d36900d10f6dbb8d1a2b829589f
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-12-27 19:10:08 -0800

    feat: error middleware

---
+++ src/middlewares/error.go
@@ -0,0 +1,47 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+const (
+	BadRequest          = "Bad Request"
+	NotAuthorized       = "Not Authorized"
+	NotFound            = "Not Found"
+	RecordNotFound      = "record not found"
+	InternalServerError = "Internal Server Error"
+)
+
+func ErrorMiddleware() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		c.Next()
+
+		err := c.Errors.ByType(gin.ErrorTypePublic).Last()
+		if err != nil {
+			log.Print(err.Err)
+
+			switch err.Error() {
+			case BadRequest:
+				c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+					"error": BadRequest,
+				})
+			case NotAuthorized:
+				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+					"error": NotAuthorized,
+				})
+			case NotFound:
+			case RecordNotFound:
+				c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+					"error": NotFound,
+				})
+			default:
+				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+					"error": InternalServerError,
+				})
+			}
+		}
+	}
+}
