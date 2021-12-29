d0c845bd57ce5d47202bd301129e74f56e05b79e
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-12-29 15:25:50 -0800

    feat: add DeleteHospital route

--- src/routers/hospital.go
+++ src/routers/hospital.go
@@ -12,5 +12,6 @@ func HospitalRoutes(rg *gin.RouterGroup) {
 		hospital.GET("/", handlers.HospitalList)
 		hospital.GET("/:hospital_id", handlers.HospitalRetrieve)
 		hospital.POST("/", handlers.HospitalRegister)
+		hospital.DELETE("/:hospital_id", handlers.HospitalDelete)
 	}
 }
