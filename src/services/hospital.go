d5e11b9f08ace272ff50a1fbb1deb75b1ad72480
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-12-29 12:57:24 -0800

    feat: add AddHospital

--- src/services/hospital.go
+++ src/services/hospital.go
@@ -3,6 +3,7 @@ package services
 import (
 	"github.com/projects/COVID_Database/src/database"
 	"github.com/projects/COVID_Database/src/models"
+	"github.com/projects/COVID_Database/src/validators"
 )
 
 type HospitalService struct{}
@@ -23,6 +24,17 @@ func (HospitalService) GetHospital(id int) (models.Hospital, error) {
 
 	hospital := models.Hospital{}
 	err := db.First(&hospital, id).Error
+
+	return hospital, err
+}
+
+func (HospitalService) AddHospital(request validators.HospitalRegisterRequest) (models.Hospital, error) {
+
+	db := database.GetDB()
+
+	hospital := validators.RegisterRequestToHospitalModel(request)
+
+	err := db.Model(models.Hospital{}).Create(&hospital).Error
 
 	return hospital, err
 }
