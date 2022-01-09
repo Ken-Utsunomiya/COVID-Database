21183f0b8f5344756c009328853d63e3237ca305
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2022-01-09 14:31:14 -0800

    feat: add RegisterRequestToDoctorModel

--- src/validators/doctor.go
+++ src/validators/doctor.go
@@ -1,5 +1,16 @@
 package validators
 
+import "github.com/projects/COVID_Database/src/models"
+
 type DoctorRegisterRequest struct {
 	Specialization string `json:"specialization" binding:"max=40"`
+}
+
+func RegisterRequestToDoctorModel(request DoctorRegisterRequest, hospitalId int) models.Doctor {
+
+	doctor := models.Doctor{}
+	doctor.HospitalID = hospitalId
+	doctor.Specialization = request.Specialization
+
+	return doctor
 }
